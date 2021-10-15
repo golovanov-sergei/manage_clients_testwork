971972701127a02b5466e0d3ddd55aafecfe62e7
Sergei <imp.sergei@gmail.com>
2021-10-15 22:55:39 +0300

    Controller added

--- src/main/java/com/manage_clients_testwork/controller/MainController.java
+++ src/main/java/com/manage_clients_testwork/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.manage_clients_testwork.controller;
 
+import com.manage_clients_testwork.model.Client;
 import com.manage_clients_testwork.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +13,22 @@ public class MainController {
     @Autowired
     private ClientService clientService;
 
+    @GetMapping("/clients")
+    public List<Client> getAllClients(){
+        return clientService.getAllCients();
+    }
+    @GetMapping("/clients/{id}")
+    public Client getClient(@PathVariable Long id){
+        return clientService.getClient(id);
+    }
+    @PostMapping("/clients")
+    public void saveClient(@RequestBody Client client){
+        clientService.validateData(client);
+        clientService.saveClient(client);
+    }
+    @PutMapping("/clients")
+    public void updateClient(@RequestBody Client client){
+        clientService.validateData(client);
+        clientService.saveClient(client);
+    }
 }
